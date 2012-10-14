f6147b4b5c79a885573724ce1faa15384458892d
Renjith I S <mailrenjithis@gmail.com>
2012-10-14 16:11:17 +0530

    Added licensing clause to scritps

--- script/reboot_to_os_installer.sh
+++ script/reboot_to_os_installer.sh
@@ -1,4 +1,35 @@
 #! /bin/bash
+#
+#	This script comes with no warranty. Use at own risk
+#
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; version 3 of the License.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program or from the site that you downloaded it
+#	from; if not, write to the Free Software Foundation, Inc., 59 Temple
+#	Place, Suite 330, Boston, MA  02111-1307   USA
+#
+#	GPL : http://www.gnu.org/copyleft/gpl.html
+#
+#	Script author : Renjith I S
+#	Webpage : http://www.renjithis.in
+#
+#	You are free to modify this script and to make it better.
+#	If you have done so and if you think you have made the script
+#	better, please mail me a copy of it or make a pull-request in GitHub.
+#	The comments provided in the script is for debugging and further development.
+#	I would also appreciate any corrections and suggestions,
+#	but i cant promise you that i would be able to implement it because of my
+#	lack of knowledge in scripting.
+#	Feel free to contact me for any information.
+#
 
 SUCCESS=0
 if [ $(which gksu) ]; then
